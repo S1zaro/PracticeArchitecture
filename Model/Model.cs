97efcb5c2348c270f904a62e7eaeb927abb5ae83
S1zaro <arikd0208@gmail.com>
2026-09-10 21:36:43 +0700

    Revert "CreateModel"
    This reverts commit dec4dcfdb746d2b3c956824de4582508c2038dba.

--- Model/Model.cs
+++ Model/Model.cs
@@ -8,21 +8,5 @@ namespace Model
 {
     internal class Figure
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Universe { get; set; }
-        public string Series { get; set; }
-        public string Character { get; set; }
-        public decimal Price { get; set; }
-
-        public Figure(int id, string name, string universe, string series, string character, decimal price)
-        {
-            Id = id;
-            Name = name;
-            Universe = universe;
-            Series = series;
-            Character = character;
-            Price = price;
-        }
     }
 }
