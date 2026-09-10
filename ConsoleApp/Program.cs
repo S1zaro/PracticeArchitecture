de080c34bb0d75f0763a963b3973a590855de622
S1zaro <arikd0208@gmail.com>
2026-09-10 21:50:05 +0700

    CreateUI
    Create UI in WinForms and ConsoleApp

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,6 +10,35 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            bool exit = false;
+            while (!exit)
+            {
+                Console.WriteLine("Приложение с коллекционными фигурками");
+                Console.WriteLine("[1] Добавить фигурку");
+                Console.WriteLine("[2] Изменить фигурку");
+                Console.WriteLine("[3] Удалить фигурку");
+                Console.WriteLine("[4] Показать фигурки из одной вселенной");
+                Console.WriteLine("[5] Сгруппировать фигурки по серии");
+                Console.WriteLine("[0] Выйти");
+                Console.Write("Ваш выбор: ");
+                string answer = Console.ReadLine();
+                switch (answer)
+                {
+                    case "1":
+                        break;
+                    case "2":
+                        break;
+                    case "3":
+                        break;
+                    case "4":
+                        break;
+                    case "5":
+                        break;
+                    case "0":
+                        exit = true;
+                        break;
+                }
+            }
         }
     }
 }
--- WinForms/MainForm.Designer.cs
+++ WinForms/MainForm.Designer.cs
@@ -28,12 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            listFigure = new ListView();
+            createButton = new Button();
+            deleteButton = new Button();
+            changeButton = new Button();
+            groupBySeriesButton = new Button();
+            showUniverse = new ComboBox();
+            SuspendLayout();
+            // 
+            // listFigure
+            // 
+            listFigure.Location = new Point(23, 88);
+            listFigure.Name = "ListFigure";
+            listFigure.Size = new Size(518, 393);
+            listFigure.TabIndex = 0;
+            listFigure.UseCompatibleStateImageBehavior = false;
+            // 
+            // createButton
+            // 
+            createButton.Location = new Point(595, 103);
+            createButton.Name = "CreateButton";
+            createButton.Size = new Size(161, 46);
+            createButton.TabIndex = 1;
+            createButton.Text = "Создать Фигурку";
+            createButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            deleteButton.Location = new Point(595, 180);
+            deleteButton.Name = "DeleteButton";
+            deleteButton.Size = new Size(161, 46);
+            deleteButton.TabIndex = 2;
+            deleteButton.Text = "Удалить Фигурку";
+            deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // changeButton
+            // 
+            changeButton.Location = new Point(595, 253);
+            changeButton.Name = "ChangeButton";
+            changeButton.Size = new Size(161, 46);
+            changeButton.TabIndex = 3;
+            changeButton.Text = "Изменить Фигурку";
+            changeButton.UseVisualStyleBackColor = true;
+            // 
+            // groupBySeriesButton
+            // 
+            groupBySeriesButton.Location = new Point(595, 331);
+            groupBySeriesButton.Name = "GroupBySeriesButton";
+            groupBySeriesButton.Size = new Size(161, 46);
+            groupBySeriesButton.TabIndex = 4;
+            groupBySeriesButton.Text = "Сгруппировать по серии";
+            groupBySeriesButton.UseVisualStyleBackColor = true;
+            // 
+            // showUniverse
+            // 
+            showUniverse.FormattingEnabled = true;
+            showUniverse.Location = new Point(595, 405);
+            showUniverse.Name = "ShowUniverse";
+            showUniverse.Size = new Size(151, 28);
+            showUniverse.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(800, 528);
+            Controls.Add(showUniverse);
+            Controls.Add(groupBySeriesButton);
+            Controls.Add(changeButton);
+            Controls.Add(deleteButton);
+            Controls.Add(createButton);
+            Controls.Add(listFigure);
+            Name = "MainForm";
             Text = "mainForm";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private ListView listFigure;
+        private Button createButton;
+        private Button deleteButton;
+        private Button changeButton;
+        private Button groupBySeriesButton;
+        private ComboBox showUniverse;
     }
 }
